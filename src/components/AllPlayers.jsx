6c1a8c1d34a3675633b2991f734d73ec32796149
meganm672 <meganm672@gmail.com>
2023-09-30 15:21:05 -0400

    worked on allplayers

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -5,19 +5,42 @@ import { useState, useEffect} from "react"
 
 const AllPlayers = () => {
     const [players, setPlayers] = useState([]);
-    
+    const [error, setError] =useState(null);
     useEffect(() =>{
-        const fetchPlayers = async () =>{
-            const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-PT-SF/players");
-
-            const players = await response.json();
-            console.log(players);
-            setPlayers(players);
+        async function fetchPlayers(){
+            try{
+                const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-PT-SF/players");
+                const playersData = await response.json();
+                console.log(playersData);
+                setPlayers(playersData);
+            }catch(e){
+                console.error(e)
+                setError(e);
+            };
         };
-        
         fetchPlayers();
-    },[])
-    return(<h1>The Allplayers component</h1>)
+    },[]);
+
+    return(
+        <div>
+            {error ? (
+                <p>{error}</p>
+            ):(
+
+                players.map((player) =>{
+                    return(
+                        <div>
+                            <h4>{player.name}</h4>
+                            <p>{player.imageUrl}</p>
+                            <p>{player.breed}</p>
+                            <p>{player.stauts}</p>
+                        </div>
+                    )
+                })
+            )
+            }
+        </div>
+    )
 }
 
 export default AllPlayers
