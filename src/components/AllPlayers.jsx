713658677f7f30200257f6b7bf5df1fb474aeef4
meganm672 <meganm672@gmail.com>
2023-09-30 14:54:10 -0400

    fecth allplayers from API

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+//import useState and useEffect hook
+import { useState, useEffect} from "react"
 
 const AllPlayers = () => {
+    const [players, setPlayers] = useState([]);
+    
+    useEffect(() =>{
+        const fetchPlayers = async () =>{
+            const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-PT-SF/players");
+
+            const players = await response.json();
+            console.log(players);
+            setPlayers(players);
+        };
+        
+        fetchPlayers();
+    },[])
     return(<h1>The Allplayers component</h1>)
 }
 
