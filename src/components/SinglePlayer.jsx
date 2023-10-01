0362ec15aeb5c715c0d39eae957c44e86f61e13f
meganm672 <meganm672@gmail.com>
2023-10-01 16:02:57 -0400

    tried to make the singleplayer form work

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -8,19 +8,20 @@ import { useParams } from "react-router-dom";
 
 
 const SinglePlayer = () => {
-    const [singlePlayer, setSinglePlayer] =useState([]);
-    const [error, setError] =useState(null);
+    const [singlePlayer, setSinglePlayer] = useState([]);
+    const [error, setError] = useState(null);
 
     const params= useParams();
 
     const playerId = params.id
+    console.log(params.id)
 
     useEffect(()=>{
         async function fetchSinglePlayer(){
             try{
                 const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-PT-SF/players/${playerId}`)
                 const data = await response.json();
-                console.log(data);
+                console.log(data.data);
                 setSinglePlayer(data);
             }catch(e){
                 console.error(e)
