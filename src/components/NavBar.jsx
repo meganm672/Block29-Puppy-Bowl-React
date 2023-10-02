61448ba7a3f5a203ba9dc1b504e9a10951494e5c
meganm672 <meganm672@gmail.com>
2023-10-02 09:16:55 -0400

    added searchbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 //import Link from react-router-dom
 import { Link } from "react-router-dom";
+import Searchbar from "./Searchbar";
 
 const Navbar = () => {
     return(
         <div>
             <Link to="/">Home</Link>
+            <Searchbar />
         </div>
     )
 }
